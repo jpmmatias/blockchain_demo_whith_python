df09796806fb767272131579ff04288e8db61bc3
João Pedro Matias <joao.matias@rebase.com.br>
2021-11-12 21:13:11 -0300

    Demo de blockchain

---
+++ main.py
@@ -0,0 +1,11 @@
+from Chain import Chain
+
+chain = Chain(10)
+i=0
+while True:
+  data = input('Add something to the chain: ')
+  chain.add_to_pool(data)
+  chain.mine()
+  if i % 5 ==0:
+    print(chain.blocks[i])
+  i += 1
